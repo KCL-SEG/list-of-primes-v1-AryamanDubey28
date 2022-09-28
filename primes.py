83052b890d0def6229d1754363a131c7730a8a71
Aryaman Dubey <89002729+AryamanDubey28@users.noreply.github.com>
2022-09-28 08:05:27 +0100

    answer

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 2
+    while len(list) != number_of_primes:
+        if isPrimeChecker(counter):
+            list.append(counter)
+        counter+=1
     return list
+
+
+def isPrimeChecker(number):
+    for i in range(1,number):
+        if number % i == 0 and i != 1:
+            return False   
+    return True
+
+
+
